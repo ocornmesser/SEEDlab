c51a7436b87e0a0725836ee8a6d1200f4d0070d9
ocornmesser <103618485+ocornmesser@users.noreply.github.com>
2023-10-27 07:54:52 -0900

    Updated calibration method

--- Demo1/DetectArucoMarker/cameraCalibration.py
+++ Demo1/DetectArucoMarker/cameraCalibration.py
@@ -11,6 +11,7 @@ import threading
 from random import random
 from smbus2 import SMBus
 import math
+import glob
 camera = cv2.VideoCapture(0) # Initialize the camera
 sleep(0.5) # wait for image to stabilize
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -18,57 +19,33 @@ objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*8,3), np.float32)
-objp[:,:2] = np.mgrid[0:6,0:8].T.reshape(-1,2)
+objp[:,:2] = np.mgrid[0:6,0:8].T.reshape(-1,2) * 2.41548
 captures = 0
-max_captures = 20 #adjust as needed
-mtxHolder = []
-while True:
-    ret,frame = camera.read() # Take an image
-    if ret:
-                
-        grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # Make the image greyscale for ArUco detection
-        ret, corners = cv2.findChessboardCorners(grey, (6,8), None) # find the marker
-        
-        if ret:
-            corners2 = cv2.cornerSubPix(grey,corners,(11,11),(-1,-1),criteria) # more exact points
-            imgpoints.append(corners2) # corners
-            objpoints.append(objp) # 3D real world points
-            captures += 1
-            cv2.drawChessboardCorners(grey, (6,8), corners2, ret) # show the corners of the chessboard on the image
-            cv2.imshow("image",grey) # show image
-            cv2.waitKey(500)
+max_captures = 30 #adjust as needed
 
-            k = cv2.waitKey(1) & 0xFF
-            
-            if k == ord('q'):
-                break
-            
-            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, grey.shape[::-1],None,None) # get camera matrix
-            print(mtx)
-            mtxHolder.append(mtx)
-            
-            if captures >= max_captures: # only take 20 captures for the average matrix
-                break
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        objp = np.zeros((6*8,3), np.float32)
-        objp[:,:2] = np.mgrid[0:6,0:8].T.reshape(-1,2)
-            
+all_images = glob.glob('capture:*.jpg')
 
+for img in all_images:
 
-avgMtx = np.zeros(mtxHolder[0].shape)
-
-skipCount = 0
-numMtx = 0
-for mtx in mtxHolder:
-    #print('in loop')
-    avgMtx += mtx
-    numMtx += 1
+    frame = cv2.imread(img)
+    grey = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # Make the image greyscale for ArUco detection
+    ret, corners = cv2.findChessboardCorners(grey, (6,8), None) # find the marker
     
-#print(f"avgMtx = {avgMtx}")
-#print(f"numMtx = {numMtx}")
-avgMtx /= numMtx
-print()
-print("average mtx:")
-print(avgMtx)
+    if ret:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(grey, corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+        cv2.drawChessboardCorners(frame, (6,8), corners2, ret)
+        cv2.imshow('img', frame)
+        cv2.waitKey(1000)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, grey.shape[::-1], None, None)
+
+print(f"calibration mtx: {mtx}")
+print(f"distortion mtx: {dist}")
+        
+            
